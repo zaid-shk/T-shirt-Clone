98ff963e123316fba2a68c6ef8e1fb10b1eb5d80
Mohammad zaid <mohammadzaid8178@gmail.com>
2025-08-26 21:16:17 +0530

    add

--- src/script.js
+++ src/script.js
@@ -1,36 +1,18 @@
-// let body = document.querySelector("body")
-// let nav = document.querySelector('#nav')
-// let page1 = document.querySelector('#page1')
-let all = document.querySelector('*')
-
-var x = document.querySelector("#sun");
-x.addEventListener('click', function () {
-    if (x.style.display === "none") {
-        x.style.display = "block";
-        moon.style.display = "none"
-        all.style.background = "black"
-        all.style.color = "#F9FAFB"
-    } else {
+let sun = document.querySelector('#dark #sun')
+let moon = document.querySelector('#dark #moon')
+sun.addEventListener('click',function(){
+    if(sun.style.display = 'none'){
         console.log("Dark mode")
-        x.style.display = "none";
-        moon.style.display = 'block'
-        console.log("hello");
-        all.style.background = "#F9FAFB"
-        all.style.color = "black"
-        document.querySelector('body').classList.toggle("dark");
+        sun.style.display = 'block'
+    }else{
+        sun.style.display = 'none'
     }
-});
-var moon = document.querySelector("#dark #moon")
-moon.addEventListener('click', function () {
-    if (moon.style.display === "none") {
-        x.style.display = "none";
-        moon.style.display = "block"
-        all.style.background = "#F9FAFB"
-        all.style.color = "black"
-    } else {
-        console.log("light mode")
-        x.style.display = "block";
+})
+moon.addEventListener('click',function(){
+    if(moon.style.display = 'none'){
+        moon.style.display = 'block'
+        console.log('')
+    }else{
         moon.style.display = 'none'
-        console.log("hello");
     }
 })

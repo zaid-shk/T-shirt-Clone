8cfbbfc1485ec1f7fed9507f1c3577efa05ea367
Mohammad zaid <mohammadzaid8178@gmail.com>
2025-08-29 11:53:48 +0530

    admin panel done

--- component/admin/script.js
+++ component/admin/script.js
@@ -31,18 +31,89 @@ new Chart(ctx, {
 let deshboard = document.querySelector('#main #Deshboard');
 let product = document.querySelector('#main #product-page');
 let order = document.querySelector('#main #order-page');
-let deshbtn = document.querySelector('#desh-btn');
-let probtn = document.querySelector('#pro-btn');
-let orderbtn = document.querySelector('#order-btn');
+let customer = document.querySelector('#main #customer-page');
+let analetics = document.querySelector('#main #analetics');
+let categories = document.querySelector('#main #categories');
+let setting = document.querySelector('#main #setting-page');
 
-probtn.addEventListener('click', function () {
-  if (deshboard.style.display = 'none') {
-    product.style.display = 'block';
-  }
+let deshbtn = document.querySelector('#deshbtn-d');
+let probtn = document.querySelector('#probtn-d');
+let orderbtn = document.querySelector('#ordbtn-d');
+let custbtn = document.querySelector('#custbtn-d');
+let anabtn = document.querySelector('#anabtn-d');
+let catebtn = document.querySelector('#catebtn-d');
+let setbtn = document.querySelector('#setbtn-d');
+let set2 = document.querySelector('#set2');
 
-});
 
-deshbtn.addEventListener('click', function () {
-  product.style.display = 'none';
-  deshboard.style.display = 'block';
-});
+function pagetoggle() {
+set2.addEventListener('click',function(){
+    if(setting.style.display = 'block'){
+            customer.style.display = 'none'
+      categories.style.display = 'none'
+      analetics.style.display = 'none'
+      product.style.display = 'none';
+      order.style.display = 'none'
+      deshboard.style.display = 'none';
+    }
+})
+
+  setbtn.addEventListener('click', function () {
+    if (setting.style.display = 'block') {
+      customer.style.display = 'none'
+      categories.style.display = 'none'
+      analetics.style.display = 'none'
+      product.style.display = 'none';
+      order.style.display = 'none'
+      deshboard.style.display = 'none';
+    }
+  });
+  catebtn.addEventListener('click', function () {
+    if (categories.style.display = 'block') {
+      customer.style.display = 'none'
+      analetics.style.display = 'none'
+      product.style.display = 'none';
+      order.style.display = 'none'
+      deshboard.style.display = 'none';
+    }
+  });
+  anabtn.addEventListener('click', function () {
+    if (analetics.style.display = 'block') {
+      customer.style.display = 'none'
+      product.style.display = 'none';
+      order.style.display = 'none'
+      deshboard.style.display = 'none';
+    }
+  });
+  custbtn.addEventListener('click', function () {
+    if (customer.style.display = 'block') {
+      product.style.display = 'none';
+      order.style.display = 'none'
+      deshboard.style.display = 'none';
+    }
+  });
+  orderbtn.addEventListener('click', function () {
+    if (order.style.display = 'block') {
+      product.style.display = 'none';
+      deshboard.style.display = 'none';
+      customer.style.display = 'none'
+    }
+  });
+  probtn.addEventListener('click', function () {
+    if (deshboard.style.display = 'none') {
+      product.style.display = 'block';
+      order.style.display = 'none'
+      customer.style.display = 'none'
+    }
+  });
+
+  deshbtn.addEventListener('click', function () {
+    product.style.display = 'none';
+    deshboard.style.display = 'block';
+    order.style.display = 'none'
+    customer.style.display = 'none'
+    setting.style.display = 'none'
+  });
+
+}
+pagetoggle() 
